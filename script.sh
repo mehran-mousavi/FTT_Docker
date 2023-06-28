83f7792d7d4453c12a1b5bfa9bf995c4ca1905cb
Mehran Mousavi <mehran.mousavi@outlook.com>
2023-06-28 17:04:13 +0330

    ssnn

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 
 echo "Running FTT..."
 
-./FTT --server --lport:443 --toip:upload.airpersian.com --toport:443 --sni:bing.com --password:123xxx
+./FTT --server --lport:443 --toip:upload.airpersian.com --toport:443 --sni:upload.airpersian.com --password:123xxx
 
 echo "FTT Is Runnig..."
